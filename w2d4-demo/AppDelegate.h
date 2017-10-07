a3dca07f31e74ccc1e549b13dad91b715ceb3f2e
Roland Tecson <roland_tecson@hotmail.com>
2017-10-06 17:03:06 -0700

    Initial Commit

---
+++ w2d4-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  w2d4-demo
+//
+//  Created by Roland on 2017-10-06.
+//  Copyright © 2017 Roland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ w2d4-demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  w2d4-demo
+//
+//  Created by Roland on 2017-10-06.
+//  Copyright © 2017 Roland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
